531120e95e62cbf338940bc1dbd3e7abc7423c1a
Vassilis <vas.terzo@gmail.com>
2019-02-05 01:47:17 +0200

    upate landing page

--- pages/index.js
+++ pages/index.js
@@ -26,70 +26,76 @@ class Index extends React.Component {
     const { classes } = this.props;
 
     return (
-      <div
-        className="container-md"
-        style={{ marginTop: 50, padding: "0 24px" }}
-      >
-        <Navbar />
-        <div className="hero">
-          Bring your ideas and let’s work together to build something beautiful
+      <React.Fragment>
+        <div className="landing1">
+          Software engineering is art and logic in balance...
         </div>
-        <div className={classnames("title", classes.title)}>
-          {/* Get in touch and book a seat to the digital transformation journey! */}
-          Reason
+        <div
+          className="container-md"
+          style={{ marginTop: "20px", padding: "0 24px" }}
+        >
+          <Navbar />
+          <div className="hero">
+            Bring your ideas and let’s work together to build something
+            beautiful
+          </div>
+          <div className={classnames("title", classes.title)}>
+            {/* Get in touch and book a seat to the digital transformation journey! */}
+            Reason
+          </div>
+          <Typography className={classes.text1}>
+            Codefully is an agile team of software engineers who love to analyze
+            business stories and know how to deliver optimal solutions. Some of
+            our key services include web and mobile application development,
+            data analytics and reporting, business process management, cloud
+            infrastructure implementation, and UI / UX design.
+            <br />
+            Read more about the{" "}
+            <Link href="/services">
+              <a className="hi">services we offer</a>
+            </Link>{" "}
+            and{" "}
+            <Link href="/services#methodology">
+              <a className="hi">our methodology</a>
+            </Link>
+            .
+          </Typography>
+          <div className={classnames("title", classes.title)}>Work</div>
+          <Typography className={classes.text1}>
+            Since 2010 we design and implement solutions for companies of all
+            sizes and from different industries. We produced applications that
+            dramatically improved aspects of business processes. We helped
+            companies turn their data into insights. We solved problems like
+            managing complicated sales contracts for distributor networks or
+            getting custom sales analytics and reports on demand and on
+            schedule. We developed customer rewards management systems, as well
+            as risk, and innovation management systems.
+            <br />
+            Read more about{" "}
+            <Link href="/work">
+              <a className="hi">our work</a>
+            </Link>{" "}
+            and view some of{" "}
+            <Link href="/work#projects">
+              <a className="hi">our projects</a>
+            </Link>
+            .
+          </Typography>
+          <div className={classnames("title", classes.title)}>Stack</div>
+          <Typography className={classes.text1}>
+            Our software stack is based on the following technologies and
+            frameworks: Ruby on Rails, React, Redux, Node.js, PostgreSQL, Amazon
+            Web Services, Heroku.
+            <br /> Read more about{" "}
+            <Link href="/stack">
+              <a className="hi">our stack</a>
+            </Link>
+            .
+          </Typography>
+          <Stack />
+          <div style={{ padding: "100px 0" }} />
         </div>
-        <Typography className={classes.text1}>
-          Codefully is an agile team of software engineers who love to analyze
-          business stories and know how to deliver optimal solutions. Some of
-          our key services include web and mobile application development, data
-          analytics and reporting, business process management, cloud
-          infrastructure implementation, and UI / UX design.
-          <br />
-          Read more about the{" "}
-          <Link href="/services">
-            <a className="hi">services we offer</a>
-          </Link>{" "}
-          and{" "}
-          <Link href="/services#methodology">
-            <a className="hi">our methodology</a>
-          </Link>
-          .
-        </Typography>
-        <div className={classnames("title", classes.title)}>Work</div>
-        <Typography className={classes.text1}>
-          Since 2010 we design and implement solutions for companies of all
-          sizes and from different industries. We produced applications that
-          dramatically improved aspects of business processes. We helped
-          companies turn their data into insights. We solved problems like
-          managing complicated sales contracts for distributor networks or
-          getting custom sales analytics and reports on demand and on schedule.
-          We developed customer rewards management systems, as well as risk, and
-          innovation management systems.
-          <br />
-          Read more about{" "}
-          <Link href="/work">
-            <a className="hi">our work</a>
-          </Link>{" "}
-          and view some of{" "}
-          <Link href="/work#projects">
-            <a className="hi">our projects</a>
-          </Link>
-          .
-        </Typography>
-        <div className={classnames("title", classes.title)}>Stack</div>
-        <Typography className={classes.text1}>
-          Our software stack is based on the following technologies and
-          frameworks: Ruby on Rails, React, Redux, Node.js, PostgreSQL, Amazon
-          Web Services, Heroku.
-          <br /> Read more about{" "}
-          <Link href="/stack">
-            <a className="hi">our stack</a>
-          </Link>
-          .
-        </Typography>
-        <Stack />
-        <div style={{ padding: "100px 0" }} />
-      </div>
+      </React.Fragment>
     );
   }
 }
