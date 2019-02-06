c329d8bc0ac198106b5e1fd08111710ee93e936c
Vassilis <vas.terzo@gmail.com>
2019-02-06 11:12:04 +0200

    update landing page

--- pages/index.js
+++ pages/index.js
@@ -9,16 +9,21 @@ import classnames from "classnames";
 import Link from "next/link";
 import { animateScroll as scroll } from "react-scroll";
 import landing1 from "../images/code-art-3.jpg";
+import { Grid } from "@material-ui/core";
 
 const styles = theme => ({
   title: {
     color: theme.palette.primary.main
   },
   text1: {
+    fontSize: "1.4rem",
+    marginTop: 5,
+    maxWidth: 900
+  },
+  text2: {
     fontSize: "1.2rem",
     marginTop: 5,
-    maxWidth: 1000,
-    lineHeight: 1.7
+    maxWidth: 900
   },
   textWhite: {
     color: "white"
@@ -57,47 +62,71 @@ class Index extends React.Component {
             <Typography className={classes.text1} style={{ marginTop: 50 }}>
               Codefully is an agile team of software engineers who love to
               analyze business stories and know how to deliver optimal
-              solutions. Some of our key services include web and mobile
-              application development, data analytics and reporting, business
-              process management, cloud infrastructure implementation, and UI /
-              UX design.
-              <br />
-              Read more about the{" "}
-              <Link href="/services">
-                <a className="hi">services we offer</a>
-              </Link>{" "}
-              and{" "}
-              <Link href="/services#methodology">
-                <a className="hi">our methodology</a>
-              </Link>
-              .
+              solutions.
             </Typography>
           </div>
         </div>
-        <div style={{ backgroundColor: "#000", padding: "5rem 0" }}>
+        <div style={{ backgroundColor: "#000" }}>
           <div className="container-md">
-            <Typography
-              className={classnames(classes.text1, classes.textWhite)}
-            >
-              Since 2010 we design and implement solutions for companies of all
-              sizes and from different industries. We produced applications that
-              dramatically improved aspects of business processes. We helped
-              companies turn their data into insights. We solved problems like
-              managing complicated sales contracts for distributor networks or
-              getting custom sales analytics and reports on demand and on
-              schedule. We developed customer rewards management systems, as
-              well as risk, and innovation management systems.
-              <br />
-              Read more about{" "}
-              <Link href="/work">
-                <a className="hi">our work</a>
-              </Link>{" "}
-              and view some of{" "}
-              <Link href="/work#projects">
-                <a className="hi">our projects</a>
-              </Link>
-              .
-            </Typography>
+            <Grid container spacing={24}>
+              <Grid item md={6} style={{ borderRight: "3px white solid" }}>
+                <div style={{ padding: "5rem 5rem 5rem 0" }}>
+                  <Typography
+                    className={classnames(classes.text1, classes.textWhite)}
+                  >
+                    Since 2010 we design and implement solutions for companies
+                    of all sizes and from different industries.
+                    <br />
+                    <br />
+                  </Typography>
+                  <Typography
+                    className={classnames(classes.text2, classes.textWhite)}
+                  >
+                    - We produced applications that dramatically improved
+                    aspects of business processes.
+                    <br />- We helped companies turn their data into insights.
+                    <br />- We solved problems like managing complicated sales
+                    contracts for distributor networks or getting custom sales
+                    analytics and reports on demand and on schedule.
+                    <br />- We developed customer rewards management systems, as
+                    well as risk, and innovation management systems.
+                  </Typography>
+                </div>
+              </Grid>
+              <Grid item md={6} style={{ borderLeft: "3px white solid" }}>
+                <div style={{ padding: "5rem 0 5rem 5rem" }}>
+                  <Typography
+                    className={classnames(classes.text1, classes.textWhite)}
+                  >
+                    Some of our key services include web and mobile application
+                    development, data analytics and reporting, business process
+                    management, cloud infrastructure implementation, and UI / UX
+                    design.
+                    <br />
+                    <br />
+                    <br />
+                    <Link href="/services">
+                      <a className="hi">Services</a>
+                    </Link>
+                    <Link href="/services#methodology">
+                      <a className="hi" style={{ marginLeft: 10 }}>
+                        Methodology
+                      </a>
+                    </Link>
+                    <Link href="/work">
+                      <a className="hi" style={{ marginLeft: 10 }}>
+                        Work
+                      </a>
+                    </Link>
+                    <Link href="/work#projects">
+                      <a className="hi" style={{ marginLeft: 10 }}>
+                        Projects
+                      </a>
+                    </Link>
+                  </Typography>
+                </div>
+              </Grid>
+            </Grid>
           </div>
         </div>
         <div className="container-md" style={{ padding: "5rem 0" }}>
