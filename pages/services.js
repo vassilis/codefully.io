1d40103ca751459000ca899a3dcad655f4a7030f
Vassilis <vas.terzo@gmail.com>
2019-02-11 14:43:24 +0200

    update services

--- pages/services.js
+++ pages/services.js
@@ -19,67 +19,60 @@ function About(props) {
   const { classes } = props;
 
   return (
-    <React.Fragment>
-      <div
-        className="container-md"
-        style={{ padding: "0 20px", margin: "0 auto 50px" }}
+    <div
+      className="container-md"
+      style={{ padding: "0 20px", margin: "0 auto 50px" }}
+    >
+      <Navbar />
+      <Typography
+        className="hero"
+        style={{ fontSize: "5rem", lineHeight: 1, margin: "50px 0" }}
       >
-        <Navbar />
-        <Typography
-          className="hero"
-          style={{ fontSize: "5rem", lineHeight: 1, margin: "50px 0" }}
-        >
-          Custom software solutions tailored to your unique needs
-        </Typography>
-        <Typography
-          style={{ fontSize: "1.2rem", marginTop: 40, maxWidth: 1000 }}
-        >
-          Expertise... experience / since 2010, rapid prototyping, MVPs, SAAS,
-          proof of concepts, customer engagement, sales analytics
-        </Typography>
-        <Typography variant="h4" className={classes.title}>
-          Methodology
-        </Typography>
-        <div style={{ fontSize: "1.2rem", marginTop: 5, maxWidth: 1000 }}>
-          {/* Agile, Scrum, TDD, Git */}
-          <p>
-            Developing software requires a combination of problem-solving as
-            well as technical skills. Figuring out how to tackle a complex
-            problem (e.g. how to help a company get certified for a particular
-            standard) often requires soft skills and thinking out of the box. We
-            regard ourselves as part of the problem-solving process. The
-            technical implementation is just the final step. Here is how we do
-            it:
-          </p>
-          <ol>
-            <li>
-              Collaboratively we go over the need that requires addressing. We
-              treat the issue holistically. The goal at this stage is to
-              identify a solution that is a good fit for the problem and which
-              takes into account the time, the budget and your long term
-              aspirations regarding the product.
-            </li>
-            <li>
-              Once we have a clear understanding and a common vision we will
-              select the appropriate technologies, design the basic architecture
-              and move as fast as possible to the initial implementation. We
-              want to keep you involved with the development process and ensure
-              your continuous engagement. We continuously deliver and follow
-              each delivery with feedback from you.
-            </li>
-            <li>
-              When the essential features are in place we will together go
-              through a release process. The manner this is done depends on the
-              nature of the product. Often a limited release will become
-              available with the goal of making minor adjustments and obtaining
-              user feedback. Together we will then ramp this up to a full
-              release.
-            </li>
-          </ol>
-        </div>
+        Custom software solutions tailored to your unique needs
+      </Typography>
+      <Typography style={{ fontSize: "1.2rem", marginTop: 40, maxWidth: 1000 }}>
+        Expertise... experience / since 2010, rapid prototyping, MVPs, SAAS,
+        proof of concepts, customer engagement, sales analytics
+      </Typography>
+      <Typography variant="h4" className={classes.title}>
+        Methodology
+      </Typography>
+      <div style={{ fontSize: "1.2rem", marginTop: 5, maxWidth: 1000 }}>
+        {/* Agile, Scrum, TDD, Git */}
+        <p>
+          Developing software requires a combination of problem-solving as well
+          as technical skills. Figuring out how to tackle a complex problem
+          (e.g. how to help a company get certified for a particular standard)
+          often requires soft skills and thinking out of the box. We regard
+          ourselves as part of the problem-solving process. The technical
+          implementation is just the final step. Here is how we do it:
+        </p>
+        <ol>
+          <li>
+            Collaboratively we go over the need that requires addressing. We
+            treat the issue holistically. The goal at this stage is to identify
+            a solution that is a good fit for the problem and which takes into
+            account the time, the budget and your long term aspirations
+            regarding the product.
+          </li>
+          <li>
+            Once we have a clear understanding and a common vision we will
+            select the appropriate technologies, design the basic architecture
+            and move as fast as possible to the initial implementation. We want
+            to keep you involved with the development process and ensure your
+            continuous engagement. We continuously deliver and follow each
+            delivery with feedback from you.
+          </li>
+          <li>
+            When the essential features are in place we will together go through
+            a release process. The manner this is done depends on the nature of
+            the product. Often a limited release will become available with the
+            goal of making minor adjustments and obtaining user feedback.
+            Together we will then ramp this up to a full release.
+          </li>
+        </ol>
       </div>
-      <Footer />
-    </React.Fragment>
+    </div>
   );
 }
 
