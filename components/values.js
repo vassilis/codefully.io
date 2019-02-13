c149df6e4e4a2c230066986e6436547db1d5a57a
Vassilis <vas.terzo@gmail.com>
2019-02-13 23:32:26 +0200

    cleanup values

--- components/values.js
+++ components/values.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { withStyles } from '@material-ui/core/styles';
 import PropTypes from 'prop-types';
-import Typography from '@material-ui/core/Typography';
 import deepPurple from '@material-ui/core/colors/deepPurple';
 
 const styles = theme => ({
@@ -45,12 +44,7 @@ const styles = theme => ({
     marginBottom: 10,
   },
   text: {
-    fontSize: '1.2rem',
-  },
-  reposition: {
-    [theme.breakpoints.up('md')]: {
-      marginTop: -60,
-    },
+    maxWidth: 900,
   },
 });
 
@@ -58,28 +52,28 @@ const Values = ({ classes }) => (
   <div className={classes.root}>
     <h2 className={classes.title}>Core Values</h2>
     <h3 className={classes.subtitle}>Solutions above technology</h3>
-    <Typography className={classes.text}>
+    <p className={classes.text}>
       We use technology for creating solutions. Technology is not a self-serving cause. Remember
       that the technology may fizzle out but the problem you are trying to solve will most likely
       still be there.
-    </Typography>
+    </p>
     <h3 className={classes.subtitle}>Opinionated development above blind implementation</h3>
-    <Typography className={classes.text}>
+    <p className={classes.text}>
       We don’t just execute. We consider our clients equal partners and we view ourselves as
       responsible for the overall success of a project. We voice our opinion and provide strong
       guidance.
-    </Typography>
+    </p>
     <h3 className={classes.subtitle}>People above projects</h3>
-    <Typography className={classes.text}>
+    <p className={classes.text}>
       We choose what we work on based on the teams that are involved. We believe that success
       requires good chemistry and we consciously spend effort building great working relationships.
-    </Typography>
+    </p>
     <h3 className={classes.subtitle}>Accessibility over rigid relationships</h3>
-    <Typography className={classes.text}>
+    <p className={classes.text}>
       We are easy to talk to and always make sure that our customers can easily reach us. Building
       software is a collaborative process and communication is key. We believe that developers and
       product owners should be in tune.
-    </Typography>
+    </p>
   </div>
 );
 
